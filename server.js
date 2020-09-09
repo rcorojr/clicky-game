7a0a148db1a821deebf7f5003386ff23949db5b7
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-09-09 11:21:46 -0600

    revert atlas

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path")
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +9,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./build"));
 }
 
+app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clicky-game");
 
--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import chars from "./chars.json";
 
+
 class App extends Component {
   constructor() {
     super();
