05ad694555be390dafe60ae520ae809c95cf851b
rcorojr <61759545+rcorojr@users.noreply.github.com>
2020-09-09 11:16:55 -0600

    revert to pre atlas fix

--- server.js
+++ server.js
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-const express = require("express");
-const path = require("path")
-const mongoose = require("mongoose");
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./build"));
-}
-
-app.get("*",(req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"))
-})
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clicky-game");
-
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
-=======
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -35,5 +10,4 @@ app.get('*', (req, res) => {
 });
 app.listen(port, () => {
    console.log('Server is up!');
-});
->>>>>>> 70f27a025cbbe3bf645712a9b798ce34c0207722
+}); 
